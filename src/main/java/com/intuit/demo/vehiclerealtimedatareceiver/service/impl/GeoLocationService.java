cb2ca391e7183f6e6ccea8d4888a10b5b05cfc93
Sunstreaker <suthagar.pp@gmail.com>
2023-10-09 13:12:03 +0530

    updated topic details

--- src/main/java/com/intuit/demo/vehiclerealtimedatareceiver/service/impl/GeoLocationService.java
+++ src/main/java/com/intuit/demo/vehiclerealtimedatareceiver/service/impl/GeoLocationService.java
@@ -22,7 +22,7 @@ public class GeoLocationService implements LocationService<Double, Double> {
     }
 
     private static double haversine(double lat1, double lon1, double lat2, double lon2) {
-        final double R = 6371.0; 
+        final double R = 6371.0;
         double dLat = Math.toRadians(lat2 - lat1);
         double dLon = Math.toRadians(lon2 - lon1);
         double a = Math.sin(dLat / 2) * Math.sin(dLat / 2)
